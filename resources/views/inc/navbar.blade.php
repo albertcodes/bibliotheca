565402d419fa4834f87a56cb3a965b78fea0314f
albertcodes <albertnthiwa@gmail.com>
2020-07-26 20:38:53 +0300

    implement search

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -22,9 +22,9 @@
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
         </li>
       </ul>
-      {{-- <form class="form-inline my-2 my-lg-0">
+      <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
         <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form> --}}
+      </form>
     </div>
   </nav>
