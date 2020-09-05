4980c9c1181c5df768f7498b3b5f5e158bc44dbb
albertcodes <albertnthiwa@gmail.com>
2020-09-05 16:35:00 +0300

    delete inForm

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,4 +10,8 @@
     <small class="text-muted">Written on: {{$post->created_at}}</small>
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-success">Edit</a>
+    {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=>'POST', 'class'=>'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
