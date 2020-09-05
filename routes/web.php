43530cf5aaf21b08ebd9b60b3360ca85d9ddbd1f
albertcodes <albertnthiwa@gmail.com>
2020-09-05 16:48:50 +0300

    Authentication scaffolding generated successfully

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
 Route::resource('/posts', 'PostsController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
