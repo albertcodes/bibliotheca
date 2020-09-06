8a9790038d60154bc5674329b19ec2673c26a6fd
albertcodes <albertnthiwa@gmail.com>
2020-09-06 19:30:59 +0300

    post details on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $user_id = auth()->user('id');
+        $user_id = auth()->user()->id;
         $user = User::find($user_id);
         return view('dashboard')->with('posts', $user->posts);
     }
