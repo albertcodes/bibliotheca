eb7b170269c0803fe5e7d9945d329a5324071d84
albertcodes <albertnthiwa@gmail.com>
2020-09-06 19:02:12 +0300

    A User may have many posts

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // A User may have many posts
+    public function posts(){
+        return  $this->hasMany('App\Post');
+    }
 }
